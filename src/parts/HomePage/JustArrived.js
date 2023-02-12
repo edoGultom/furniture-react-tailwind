d867f5c600295fd95451fd721197494f4405ca3f
Dwi Herydo Gultom <edogultom10395@gmail.com>
2023-02-12 21:20:02 +0700

    add: skeleton justArrived

--- src/parts/HomePage/JustArrived.js
+++ src/parts/HomePage/JustArrived.js
@@ -10,6 +10,10 @@ function Loading() {
     .map((_, index) => {
       return (
         <div className="px-4 relative card group" key={index}>
+          <div
+            className="rounded-xl bg-gray-300 overflow-hidden card-shadow relative"
+            style={{ width: 287, height: 386 }}
+          ></div>
           <div className="w-24 h-3 bg-gray-300 mt-3 rounded-full"></div>
           <div className="w-36 h-3 bg-gray-300 mt-2 rounded-full"></div>
         </div>
