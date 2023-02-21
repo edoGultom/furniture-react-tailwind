2cb4113c3e925dd081bc897ab5d4b81216f2f34b
Dwi Herydo Gultom <edogultom10395@gmail.com>
2023-02-22 00:06:54 +0700

    update: perbaikan humberger

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -22,7 +22,7 @@ export default function Header({ theme, position }) {
   }, [state.cart]);
 
   return (
-    <header className={[position, "w-full z-20 px-4"].join(" ")}>
+    <header className={[position, "w-full z-40 px-4"].join(" ")}>
       <div className="container mx-auto py-5">
         <div className="flex flex-stretch items-center">
           <div className="w-56 items-center flex">
@@ -36,7 +36,12 @@ export default function Header({ theme, position }) {
           <div className="w-full"></div>
           <div className="w-auto">
             <ul
-              className="fixed bg-white inset-0 flex flex-col invisible items-center justify-center opacity-0 md:visible md:flex-row md:bg-transparent md:relative md:opacity-100 md:flex md:items-center"
+              className={[
+                "fixed bg-white inset-0 flex flex-col items-center justify-center  md:visible md:flex-row md:bg-transparent md:relative md:opacity-100 md:flex md:items-center",
+                ToggleMainMenu
+                  ? "opacity-100 z-30 visible"
+                  : "invisible opacity-0",
+              ].join(" ")}
               id="menu"
             >
               <li className="mx-3 py-6 md:py-0">
@@ -46,7 +51,7 @@ export default function Header({ theme, position }) {
                     "hover:underline",
                     theme === "white"
                       ? "text-black md:text-white"
-                      : "text-white md:text-black",
+                      : "text-black md:text-black",
                   ].join(" ")}
                 >
                   Showcase
@@ -59,7 +64,7 @@ export default function Header({ theme, position }) {
                     "hover:underline",
                     theme === "white"
                       ? "text-black md:text-white"
-                      : "text-white md:text-black",
+                      : "text-black md:text-black",
                   ].join(" ")}
                 >
                   Catalog
@@ -72,7 +77,7 @@ export default function Header({ theme, position }) {
                     "hover:underline",
                     theme === "white"
                       ? "text-black md:text-white"
-                      : "text-white md:text-black",
+                      : "text-black md:text-black",
                   ].join(" ")}
                 >
                   Delivery
@@ -85,7 +90,7 @@ export default function Header({ theme, position }) {
                     "hover:underline",
                     theme === "white"
                       ? "text-black md:text-white"
-                      : "text-white md:text-black",
+                      : "text-black md:text-black",
                   ].join(" ")}
                 >
                   Rewards
@@ -97,8 +102,14 @@ export default function Header({ theme, position }) {
             <ul className="items-center flex">
               <li className="ml-6 block md:hidden">
                 <button
-                  id="menu-toggler"
-                  className="relative flex z-50 items-center justify-center w-8 h-8 text-black md:text-white focus:outline-none"
+                  className={[
+                    "flex z-50 items-center justify-center w-8 h-8 text-black md:text-white focus:outline-none",
+                    ToggleMainMenu ? "fixed top-0 right-0" : "relative",
+                    theme === "white"
+                      ? "text-black md:text-white"
+                      : "text-black md:text-black",
+                  ].join(" ")}
+                  onClick={() => setToggleMainMenu((prev) => !prev)}
                 >
                   <svg
                     className="fill-current"
@@ -118,7 +129,7 @@ export default function Header({ theme, position }) {
                     "cart flex items-center justify-center w-8 h-8",
                     theme === "white"
                       ? "text-black md:text-white"
-                      : " text-white md:text-black",
+                      : " text-black md:text-black",
                     state.cart && Object.keys(state.cart).length > 0
                       ? "cart-filled"
                       : "",
